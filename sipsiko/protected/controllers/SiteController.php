2dc68244141abbc90d9c2ec29024909cd45b4d5f
Mahendri Winata <mahen.0112@gmail.com>
2014-03-29 05:16:12 +0700

    change login controller

--- sipsiko/protected/controllers/SiteController.php
+++ sipsiko/protected/controllers/SiteController.php
@@ -15,22 +15,4 @@ class SiteController extends GuestController {
         }
     }
 
-    public function actionLogin() {
-        $model = new LoginForm;
-
-        $this->performAjaxValidation($model, 'login-form');
-
-        if (isset($_POST['LoginForm'])) {
-            $model->attributes = $_POST['LoginForm'];
-            if ($model->validate() && $model->login())
-                $this->redirect(Yii::app()->user->returnUrl);
-        }
-        $this->render('login', array('model' => $model));
-    }
-
-    public function actionLogout() {
-        Yii::app()->user->logout();
-        $this->redirect(Yii::app()->homeUrl);
-    }
-
 }
