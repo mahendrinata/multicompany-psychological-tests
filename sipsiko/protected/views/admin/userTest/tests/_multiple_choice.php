a3444ef1b6eaa050b4124ecf72326ca6f48707ab
Mahendri Winata <mahen.0112@gmail.com>
2014-04-21 03:18:12 +0700

    test status checker

--- sipsiko/protected/views/admin/userTest/tests/_multiple_choice.php
+++ sipsiko/protected/views/admin/userTest/tests/_multiple_choice.php
@@ -3,44 +3,48 @@
     $defaultAnswer = TestAnswer::model()->getDefaultAnswer($model->id);
     $i = 1;
     foreach ($model->test->questions as $question) {
-        ?>
-        <div class="col-lg-4 col-sm-6 col-xs-12" id="question-block-<?php echo $i; ?>">
-            <div class= "block">
-                <p>
-                    <label class="radio">
-                        <span class="number-question"><?php echo $i; ?></span>
-                        <?php echo $question->description; ?>
-                    </label>
-                    <?php
-                    $default = (isset($defaultAnswer[$question->id]) && !empty($defaultAnswer[$question->id])) ? $defaultAnswer[$question->id] : null;
-                    foreach ($question->answers as $answer) {
-                        ?>
+        if ($question->status == Status::ACTIVE) {
+            ?>
+            <div class="col-lg-4 col-sm-6 col-xs-12" id="question-block-<?php echo $i; ?>">
+                <div class= "block">
+                    <p>
                         <label class="radio">
-                            <?php
-                            echo CHtml::radioButton('UserTest[question][' . $question->id . ']', ($default == $answer->id) ? true : false, array(
-                                'required' => 'required',
-                                'data-user-test' => $model->id,
-                                'data-question' => $question->id,
-                                'data-answer' => $answer->id,
-                                'data-token' => TestAnswer::model()->generateToken($model->token, $answer->id),
-                                'value' => $answer->id,
-                                'class' => 'answers'));
-                            echo $answer->description;
-                            ?>
+                            <span class="number-question"><?php echo $i; ?></span>
+                            <?php echo $question->description; ?>
                         </label>
                         <?php
-                    }
-                    ?>
-                </p>
+                        $default = (isset($defaultAnswer[$question->id]) && !empty($defaultAnswer[$question->id])) ? $defaultAnswer[$question->id] : null;
+                        foreach ($question->answers as $answer) {
+                            if ($answer->status == Status::ACTIVE) {
+                                ?>
+                                <label class="radio">
+                                    <?php
+                                    echo CHtml::radioButton('UserTest[question][' . $question->id . ']', ($default == $answer->id) ? true : false, array(
+                                        'required' => 'required',
+                                        'data-user-test' => $model->id,
+                                        'data-question' => $question->id,
+                                        'data-answer' => $answer->id,
+                                        'data-token' => TestAnswer::model()->generateToken($model->token, $answer->id),
+                                        'value' => $answer->id,
+                                        'class' => 'answers'));
+                                    echo $answer->description;
+                                    ?>
+                                </label>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </p>
+                </div>
             </div>
-        </div>
-        <?php
-        if ($i % 2 == 0 && $i != 0)
-            echo '<div class="clearfix clearfix-display clearfix-sm"></div>';
+            <?php
+            if ($i % 2 == 0 && $i != 0)
+                echo '<div class="clearfix clearfix-display clearfix-sm"></div>';
 
-        if ($i % 3 == 0 && $i != 0)
-            echo '<div class="clearfix clearfix-display clearfix-lg"></div>';
-        $i++;
+            if ($i % 3 == 0 && $i != 0)
+                echo '<div class="clearfix clearfix-display clearfix-lg"></div>';
+            $i++;
+        }
     }
     ?>
 </div>
