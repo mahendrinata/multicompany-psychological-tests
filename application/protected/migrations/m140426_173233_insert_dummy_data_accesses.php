2a31a722932ed0ed4ae21136fb3d3359d8202275
Mahendri Winata <mahen.0112@gmail.com>
2014-04-27 01:31:40 +0700

    add data validation expert test

--- application/protected/migrations/m140426_173233_insert_dummy_data_accesses.php
+++ application/protected/migrations/m140426_173233_insert_dummy_data_accesses.php
@@ -3,13 +3,265 @@
 class m140426_173233_insert_dummy_data_accesses extends CDbMigration {
 
     public function up() {
-        $user = User::model()->findByAttributes(array('username' => 'mahendri'));
+        $user = User::model()->findByAttributes(array('username' => 'admin'));
+        $positions = CHtml::listData(Position::model()->findAll(), 'slug', 'id');
         $row = array(
             array(
-                'slug' => Access::slugify(''),
-                'name' => ucwords(Status::DRAFT),
-                'url' => '',
-                'params' => '',
+                'slug' => Access::slugify('admin/answer/create'),
+                'name' => 'Create Answer',
+                'url' => 'admin/answer/create',
+                'params' => 'id:true;type_id:true',
+                'Positions' => array(
+                    $positions[Role::MEMBER . '-' . Position::MEMBER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/answer/delete'),
+                'name' => 'Delete Answer',
+                'url' => 'admin/answer/delete',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::MEMBER . '-' . Position::MEMBER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/answer/delete'),
+                'name' => 'Delete Answer',
+                'url' => 'admin/answer/delete',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::MEMBER . '-' . Position::MEMBER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/answer/delete'),
+                'name' => 'Delete Answer',
+                'url' => 'admin/answer/delete',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::MEMBER . '-' . Position::MEMBER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/dashboard'),
+                'name' => 'Dashboard Index',
+                'url' => 'admin/dashboard',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::ADMIN.'-'.Position::ADMIN],
+                    $positions[Role::EXPERT.'-'.Position::OWNER],
+                    $positions[Role::COMPANY.'-'.Position::OWNER],
+                    $positions[Role::MEMBER . '-' . Position::MEMBER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/dashboard/index'),
+                'name' => 'Dasboard Index',
+                'url' => 'admin/dashboard/index',
+                'Positions' => array(
+                    $positions[Role::ADMIN.'-'.Position::ADMIN],
+                    $positions[Role::EXPERT.'-'.Position::OWNER],
+                    $positions[Role::COMPANY.'-'.Position::OWNER],
+                    $positions[Role::MEMBER . '-' . Position::MEMBER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/question/create'),
+                'name' => 'Create Question',
+                'url' => 'admin/question/create',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/question/update'),
+                'name' => 'Update Question',
+                'url' => 'admin/question/update',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/question/delete'),
+                'name' => 'Delete Question',
+                'url' => 'admin/question/delete',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/role'),
+                'name' => 'Role Index',
+                'url' => 'admin/role',
+                'Positions' => array(
+                    $positions[Role::ADMIN . '-' . Position::ADMIN],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/role/index'),
+                'name' => 'Role Index',
+                'url' => 'admin/role/index',
+                'Positions' => array(
+                    $positions[Role::ADMIN . '-' . Position::ADMIN],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/role/view'),
+                'name' => 'View Role',
+                'url' => 'admin/role/view',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::ADMIN . '-' . Position::ADMIN],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/role/create'),
+                'name' => 'Create Role',
+                'url' => 'admin/role/create',
+                'Positions' => array(
+                    $positions[Role::ADMIN . '-' . Position::ADMIN],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/role/update'),
+                'name' => 'Update Role',
+                'url' => 'admin/role/update',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::ADMIN . '-' . Position::ADMIN],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/role/delete'),
+                'name' => 'Delete Role',
+                'url' => 'admin/role/delete',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::ADMIN . '-' . Position::ADMIN],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/tag'),
+                'name' => 'Tag Index',
+                'url' => 'admin/tag',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/tag/index'),
+                'name' => 'Tag Index',
+                'url' => 'admin/tag/index',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/tag/view'),
+                'name' => 'View Tag',
+                'url' => 'admin/tag/view',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/tag/create'),
+                'name' => 'Create Tag',
+                'url' => 'admin/tag/create',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/tag/update'),
+                'name' => 'Update Tag',
+                'url' => 'admin/tag/update',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/tag/delete'),
+                'name' => 'Delete Tag',
+                'url' => 'admin/tag/delete',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/test'),
+                'name' => 'Test Expert Index',
+                'url' => 'admin/test',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/test/index'),
+                'name' => 'Test Expert Index',
+                'url' => 'admin/test/index',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/test/view'),
+                'name' => 'View Expert Test',
+                'url' => 'admin/test/view',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/test/create'),
+                'name' => 'Create Expert Test',
+                'url' => 'admin/test/create',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/test/update'),
+                'name' => 'Update Expert Test',
+                'url' => 'admin/test/update',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/test/delete'),
+                'name' => 'Delete Expert Test',
+                'url' => 'admin/test/delete',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/test/generatevalidation'),
+                'name' => 'Generate Validation Test',
+                'url' => 'admin/test/generatevalidation',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
+            ),
+            array(
+                'slug' => Access::slugify('admin/test/result'),
+                'name' => 'Result Test',
+                'url' => 'admin/test/result',
+                'params' => 'id:true',
+                'Positions' => array(
+                    $positions[Role::EXPERT . '-' . Position::OWNER],
+                )
             ),
         );
         foreach ($row as $column) {
